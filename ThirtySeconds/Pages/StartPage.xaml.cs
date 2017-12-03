fcc7a12a528395bd6f4a69f5d73b6f8949808a23
Zeyad Atef <zeatef@gmail.com>
2017-12-03 13:33:48 +0200

    Initial Project, Launch Screen and Start Page(Needs work)

---
+++ ThirtySeconds/Pages/StartPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ThirtySeconds
+{
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
