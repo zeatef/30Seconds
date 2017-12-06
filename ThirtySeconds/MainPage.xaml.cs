c14f53c1ab5a465cd4d451f0c9390eb4c0ffb88c
Zeyad Atef <zeatef@gmail.com>
2017-12-06 16:00:23 +0200

    Added icons in tab bar

--- ThirtySeconds/MainPage.xaml.cs
+++ ThirtySeconds/MainPage.xaml.cs
@@ -12,12 +12,18 @@ namespace ThirtySeconds
             InitializeComponent();
             this.Title = this.CurrentPage.Title;
             this.CurrentPageChanged += CurrentPageHasChanged;
+            this.Focused += FocusedBar;
 
         }
 
         protected void CurrentPageHasChanged(object sender, EventArgs e)
         {
             this.Title = this.CurrentPage.Title;
+        }
+
+        protected void FocusedBar(object sender, EventArgs e)
+        {
+            this.BarTextColor = Color.Green;
         }
     }
 }
--- ThirtySeconds/obj/Debug/ThirtySeconds.OrderPage.xaml.g.cs
+++ ThirtySeconds/obj/Debug/ThirtySeconds.OrderPage.xaml.g.cs
@@ -15,8 +15,12 @@ namespace ThirtySeconds {
     public partial class OrderPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Label welcomeLabel;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(OrderPage));
+            welcomeLabel = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "welcomeLabel");
         }
     }
 }
